67a96ea4c6a77cbda482831bbe5b8f2c9b0743d5
sherapatrejepova <st.rejepova@gmail.com>
2021-06-17 17:56:05 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+
+const array = [];
+
+function writeCards(name, event) {
+    for (let i = 0; i < name.length; i++) {
+        array.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`);
+        debugger;
+    }
+    return array;
+}
+
+function countDown(init = 10) {
+    while (init >= 0) {
+        console.log(init--);
+    }
+}
+
